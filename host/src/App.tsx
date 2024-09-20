74499d8d3b28731b1c4301a140774306ea4e1a8e
zmzlois <lois.z@comcord.vision>
2024-09-20 10:16:35 +0100

    push

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,6 +1,6 @@
 import { lazy, Suspense } from 'react';
 // import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
+import viteLogo from '/vite.svg';
 
 import './App.css';
 //@ts-expect-error - Remote
@@ -26,10 +26,13 @@ function App() {
           <RspackImage />
         </Suspense>
         <p>
+          <img src={viteLogo} alt="Vite logo" />
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
